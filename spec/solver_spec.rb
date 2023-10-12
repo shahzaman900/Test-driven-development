ab1ef3b131c58efa55f7de5caae29000722ea1fd
anshuman7negi <anshumannegi777@gmail.com>
2023-10-12 19:49:15 +0530

    fixed issues

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -11,7 +11,7 @@ describe Solver do
       expect(method.factorial(0)).to eq(1)
     end
     it 'negative integer raise an exception.' do
-      expect(method.factorial(-3)).to raise(ArgumentError)
+        expect { method.factorial(-3) }.to raise_error(ArgumentError)
     end
   end
 
@@ -26,13 +26,13 @@ describe Solver do
       expect(method.fizzbuzz(3)).to eq('fizz')
     end
     it 'return buzz when number is divisible only by 5' do
-      expect(method.factorial(5)).to eq('buzz')
+      expect(method.fizzbuzz(5)).to eq('buzz')
     end
     it 'return fizzbuzz when number is divisible  by 3 and 5' do
-      expect(method.factorial(15)).to eq('fizzbuzz')
+      expect(method.fizzbuzz(15)).to eq('fizzbuzz')
     end
     it 'return number when number is not divisible  by both 3 and 5 or either' do
-      expect(subject.fizzbuzz(10)).to eq('10')
+      expect(subject.fizzbuzz(7)).to eq('7')
     end
   end
 end
